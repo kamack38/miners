dc3e9e5c8b11e374c64eb1ac06051a4ef7f53180
kamack38 <64226248+kamack38@users.noreply.github.com>
2023-05-12 17:36:36 +0200

    tmp

--- crates/miners-protocol/src/utils/slot.rs
+++ crates/miners-protocol/src/utils/slot.rs
@@ -1,4 +1,7 @@
-use crate::utils::nbt::NBTType;
+use crate::{
+    packet::{IntoPacket, RawPacket},
+    utils::nbt::NBTType,
+};
 
 #[derive(Debug, Clone)]
 pub struct Slot {
@@ -7,3 +10,29 @@ pub struct Slot {
     pub item_count: Option<u8>,
     pub nbt: Option<NBTType>,
 }
+
+impl Slot {
+    pub fn from_packet(packet: &mut RawPacket) -> Result<Slot, String> {
+        let present = packet.read_bool();
+        Ok(Slot {
+            present,
+            item_id: match present {
+                true => Some(packet.read_int()),
+                false => None,
+            },
+            item_count: match present {
+                true => Some(packet.read_byte()),
+                false => None,
+            },
+            nbt: match present {
+                true => Some(match NBTType.from_packet(packet) {
+                    Ok(nbt) => nbt,
+                    Err(e) => return e,
+                }),
+                false => None,
+            },
+        })
+    }
+}
+
+impl IntoPacket for Slot {}
